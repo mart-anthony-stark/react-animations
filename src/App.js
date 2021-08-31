272b1955fd5ef234db160c918365e92c3f5aa260
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-31 13:08:08 -0700

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 import AOS from 'aos';
 import Component1 from './components/Component1';
+import useMeasure from './useMeasure';
 import { useSpring, animated } from 'react-spring';
 
 export default function App() {
   const [active, setActive] = useState(false);
+  const [bind, { width }] = useMeasure();
   const props = useSpring({
     from: { width: 0 },
-    to: { width: '100%' }
+    width
   });
 
-  // useEffect(() => {
-  //   AOS.init();
-  // }, []);
-
   return (
-    <div className="main">
-      <animated.div class="fill" style={{ width: props }} />
+    <div {...bind} class="main" onClick={() => toggle(!open)}>
+      <animated.div class="fill" style={props} />
+      <animated.div class="content">
+        {props.width.interpolate(x => Math.floor((x.toFixed(0) * 100) / width))}
+      </animated.div>
     </div>
   );
 }
