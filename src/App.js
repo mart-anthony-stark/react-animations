802000cb94a80cc22e82741d0ab8eca74c74b98f
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-31 14:01:59 -0700

    4 files modified

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 import AOS from 'aos';
 import Component1 from './components/Component1';
+import Tabs from './components/Tabs';
 import useMeasure from './useMeasure';
 import { useSpring, animated } from 'react-spring';
 
 export default function App() {
-  const [open, toggle] = useState(false);
-  const [bind, { width }] = useMeasure();
-  const props = useSpring({
-    width: open ? width : 0,
-    backgroundColor: open ? 'hotpink' : 'turquoise',
-    config: { duration: 1000 }
-  });
+  // const [open, toggle] = useState(false);
+  // const [bind, { width }] = useMeasure();
+  // const props = useSpring({
+  //   width: open ? width : 0,
+  //   backgroundColor: open ? 'hotpink' : 'turquoise',
+  //   config: { duration: 1000 }
+  // });
 
   return (
-    <div {...bind} class="main" onClick={() => toggle(!open)}>
-      <animated.div class="fill" style={props} />
-      <animated.div class="content">
-        {props.width.interpolate(x => Math.floor((x.toFixed(0) * 100) / width))}
-      </animated.div>
+    <div>
+      <Tabs />
     </div>
   );
 }
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,29 +4,27 @@ import '../styles/tab.css';
 export default function Tabs() {
   const [tab, setTab] = useState(1);
 
-  const togleTab = tab => {
-    setTab(Tab);
+  const toggleTab = tab => {
+    console.log(tab);
+    setTab(tab);
   };
 
   return (
     <div className="tabs">
+      <nav>
+        <button onClick={() => toggleTab(1)}>Tab1</button>
+        <button onClick={() => toggleTab(2)}>Tab2</button>
+        <button onClick={() => toggleTab(3)}>Tab3</button>
+      </nav>
+
       <div className="content1" className={tab == 1 ? 'active' : 'hide'}>
-        <div className="content">
-          Tab1 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Eligendi, obcaecati!
-        </div>
+        <div className="tab">Tab1</div>
       </div>
-      <div className="content2" className={tab == 1 ? 'active' : 'hide'}>
-        <div className="content">
-          Tab2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Eligendi, obcaecati!
-        </div>
+      <div className="content2" className={tab == 2 ? 'active' : 'hide'}>
+        <div className="tab">Tab2</div>
       </div>
-      <div className="content3" className={tab == 1 ? 'active' : 'hide'}>
-        <div className="content">
-          Tab3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Eligendi, obcaecati!
-        </div>
+      <div className="content3" className={tab == 3 ? 'active' : 'hide'}>
+        <div className="tab">Tab3</div>
       </div>
     </div>
   );
