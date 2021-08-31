eb228017f6fda05c90bd2a82bd757c9ba8a0d7bb
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-31 13:35:34 -0700

    1 file modified, 1 added

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/tab.css';
 
 export default function Tabs() {
-  return(
+  const [tab, setTab] = useState(1);
+
+  const togleTab = tab => {
+    setTab(Tab);
+  };
+
+  return (
     <div className="tabs">
-      
+      <div className="content1" className={tab == 1 ? 'active' : 'hide'}>
+        <div className="content">
+          Tab1 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Eligendi, obcaecati!
+        </div>
+      </div>
+      <div className="content2" className={tab == 1 ? 'active' : 'hide'}>
+        <div className="content">
+          Tab2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Eligendi, obcaecati!
+        </div>
+      </div>
+      <div className="content3" className={tab == 1 ? 'active' : 'hide'}>
+        <div className="content">
+          Tab3 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Eligendi, obcaecati!
+        </div>
+      </div>
     </div>
-  )
+  );
 }
