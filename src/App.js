1588b43f854399a7a19237d58d371d32edb6f0a7
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-31 13:09:13 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import useMeasure from './useMeasure';
 import { useSpring, animated } from 'react-spring';
 
 export default function App() {
-  const [active, setActive] = useState(false);
+  const [open, toggle] = useState(false);
   const [bind, { width }] = useMeasure();
   const props = useSpring({
-    from: { width: 0 },
-    width
+    width: open ? width : 0,
+    backgroundColor: open ? 'hotpink' : 'turquoise',
+    config: { duration: 1000 }
   });
 
   return (
     <div {...bind} class="main" onClick={() => toggle(!open)}>
+      <Component1 />
       <animated.div class="fill" style={props} />
       <animated.div class="content">
         {props.width.interpolate(x => Math.floor((x.toFixed(0) * 100) / width))}
