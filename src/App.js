23c8c4107ae7fed4d28bc0d54dcebc7f4ddf7b61
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-31 13:02:27 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import AOS from 'aos';
 import Component1 from './components/Component1';
+import { useSpring, animated } from 'react-spring';
 
 export default function App() {
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    from: { width: 0 },
+    to: { width: '100%' }
+  });
+
   useEffect(() => {
     AOS.init();
   }, []);
 
   return (
-    <div className="app">
-      <Component1 />
+    <div className="main">
+      <animated.div class="fill" style={{ width: props }} />
     </div>
   );
 }
